47d2888591b780cd81e42b338c4c848532ab2acc
HashBR <tulio.bart@msn.com>
2022-03-14 19:05:25 -0300

    Better input validation.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react"
+import React, { useEffect, useState } from "react"
 import CardList from './CardList';
 import {v4 as uuidv4} from 'uuid'
 
@@ -12,7 +12,7 @@ function App() {
   const  [atk, setAtk] = useState("");
   const  [def, setDef] = useState("");
 
-  useEffect(() => {
+   useEffect(() => {
     const storedCards = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
     if (storedCards)
       setCards(storedCards)
@@ -24,17 +24,44 @@ function App() {
 
   function handleAddCard(e) {
     e.preventDefault()
-    if (!name || !desc || !atk || !def){
-      alert(`Faltou algum campo obrigatório.`)
-    }
-    else{
-      clearForm()
-      alert(`Eita porra! É o ${name} ${desc}! Ele tem ${atk}/${def}`);
-    }
-
+    // Codigo comentado para exibir alerta se clicasse em submit quando faltava algo.
+    // Agora ele só verifica no submit se tem alguma coisa faltando.
+    // if (requiredChecker()){
+    //   if (requiredChecker().length === "1"){
+    //     alert(`Faltou o seguinte campo: ${requiredChecker()}`)
+    //   }
+    //   if (requiredChecker().length > "1"){
+    //     alert(`Faltaram os seguintes campos: ${requiredChecker().join(", ")}`)
+    //   }
+    // }
+    //else{
+    clearForm()
+    //alert(`Eita porra! É o ${name} ${desc}! Ele tem ${atk}/${def}`);
     setCards(prevCards => {
       return [...prevCards, { id: uuidv4(), name: name, desc: desc, atk: atk, def: def}]
-    })
+      })
+    //} 
+  }
+
+// Devolve uma array com o que falta
+  function requiredChecker() {
+    var missinginputs = []
+    if (!name){
+      missinginputs.push("Nome")
+    }
+    if (!desc){
+      missinginputs.push("Descrição")
+    }
+    if (!atk){
+      missinginputs.push("Ataque")
+    }
+    if (!def){
+      missinginputs.push("Defesa")
+    }
+    else {
+      missinginputs = ""
+    }
+    return missinginputs
   }
 
   function clearForm() {
@@ -48,9 +75,14 @@ function App() {
     const newCards = cards.filter(card => !card);
     e.preventDefault();
     setCards(newCards)
-    alert("Limpado.")
+    console.log("clear")
+    //alert("Limpado.")
   }
-  
+
+  function descLimiter() {
+    return (desc.length > 200)
+  }
+
   return (
     <div className="card align-middle">
       <div className="card-body">
@@ -62,23 +94,25 @@ function App() {
                 <input type="text" className="form-control" id="card-input" placeholder="Rei Caveira" onChange={(e) => setName(e.target.value)} value={name}/>
               </div>
               <div className="col-md-12 mb-12" >
-                <label className="form-label" htmlFor="desc-input">Descrição</label>
+                <label className="col-11 form-label" htmlFor="desc-input">Descrição</label>
+                <span className="col-1">{desc.length}</span>
                 <textarea type="text" className="form-control" id="desc-input" placeholder="Spooky Scary Skeleton" onChange={(e) => setDesc(e.target.value)} value={desc} />
+                {descLimiter() && <span className="text-danger">Estourado o limite de caracteres!</span>}
               </div>
             </div>
 
             <div className="row g-2">
               <div className="col-md-6 mb-3">
                 <label className="form-label" htmlFor="atk-input">Ataque</label>
-                <input type="number" className="form-control" id="atk-input" placeholder="2500" onChange={(e) => setAtk(e.target.value)} value={atk} />
+                <input type="text" className="form-control" id="atk-input" placeholder="2500" onChange={(e) => setAtk(e.target.value.replace(/[^0-9]/g, ""))} value={atk} />
               </div>
               <div className="col-md-6 mb-3">
                 <label className="form-label" htmlFor="def-input">Defesa</label>
-                <input type="number" className="form-control" id="def-input" placeholder="1250" onChange={(e) => setDef(e.target.value)} value={def} />
+                <input type="text" className="form-control" id="def-input" placeholder="1250" onChange={(e) => setDef(e.target.value.replace(/[^0-9]/g, ""))} value={def} />
               </div>
             </div>
-            <button className="btn btn-primary" type="submit">Salvar</button>
-            <button className="btn btn-secondary" onClick={handleClearCard}>Limpar</button>
+            <button className="btn btn-primary" disabled={requiredChecker() || descLimiter()} type="submit">Salvar</button>
+            <button className="btn btn-secondary" onClick={handleClearCard}>Apagar cartas salvas</button>
             <div className="row">
               <div className="col-md-6 mb-3">
                 <CardList cards={cards}  />
